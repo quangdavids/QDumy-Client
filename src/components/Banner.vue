08f9b9637a3e5cf5d62c4541cd9a051a654d5782
Quang Pham <jeejkkoo@gmail.com>
2026-04-26 14:13:28 +0700

    fix

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -7,7 +7,7 @@
   <div class="bg-black z-0 ">
  <div class="relative  max-w-auto max-h-300 flex container w-full text-white mx-auto">
   <div class=" md:w-1/2   flex sm:w-screen flex-col gap-5 justify-center mb-4 p-3 mx-auto">
-    <h1 class="text-[60px] font-bold"> QuangStudy</h1>
+    <h1 class="text-[60px] font-bold"> QDumy</h1>
     <p class="text-4xl "> The Best Place For The Best <span class="underline text-green-400">Career</span>.</p>
     <p class="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis placerat arcu nibh sed placerat elementum. Urna eu et purus et mi sit rhoncus. Id nisl quis arcu nulla.</p>
     <div>
